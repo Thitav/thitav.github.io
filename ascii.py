8fdfb8e6c837ba6d18a1379c549df00be35a3c5c
Thiago Tavares <thiptav@gmail.com>
2024-09-08 03:38:07 -0300

    First commit

---
+++ ascii.py
@@ -0,0 +1,12 @@
+with open("ascii.txt", "rb") as f:
+    ascii = f.read()
+    f.close()
+
+ascii = ascii.replace(b"\xe2\x96\x88", b" ")
+ascii = ascii.replace(b"\xe2\x96\x91", b" ")
+ascii = ascii.replace(b"\xe2\x96\x92", b"\xe2\x96\x88")
+ascii = ascii.replace(b"\xe2\x96\x93", b"\xe2\x96\x92")
+
+with open("newascii.txt", "wb") as f:
+    f.write(ascii)
+    f.close()
